fdf5028ac6ee88c03dbcf860dc7cd7a1ab7a961d
AlanLeeson <aml9227@rit.edu>
2016-05-06 15:23:03 -0400

    Started quiz mechanics

--- index.js
+++ index.js
@@ -2,8 +2,33 @@
 
 (function(){
 	
+	var words = [];
+	var category = "";
+	
 	window.onload = function(){
 		console.log("Started");
+		addWord("Alan");
+		addWord("Maria");
+		addWord("Bob");
+		
+		$("#quiz_word").text(getWord());
+		
+		$(".select_button").bind('click',function(){
+			$("#quiz_word").text(getWord());
+		});
+		
+	}
+	
+	function addWord(word){
+		words.push(word);
+	}
+	
+	function getWord(){
+		if(words.length > 0){
+			return words.splice(Math.random()*words.length,1)[0]
+		}else{
+			return "Done.";
+		}
 	}
 	
 })();
