0998898c44189d6c07c34916cd334230e9701376
AlanLeeson <aml9227@rit.edu>
2016-05-09 16:33:36 -0400

    Made the site walk through with a few tweeks to css

--- index.js
+++ index.js
@@ -11,6 +11,12 @@
 	
 	window.onload = function(){
 
+		if(location.hash.slice(1) == ''){
+			window.location.href = window.location.href + "#1";
+		}else{
+			console.log(location.hash.slice(1));
+			$('[name='+location.hash.slice(1)+']').show();
+		}
 		setUpQuiz();
 		
 		$("#left_category_button").text(leftCategory);
@@ -20,7 +26,26 @@
 		$(".select_button").bind('click',function(){
 			$("#quiz_word").text(getWord());
 		});
+		makeNewScreen();
 		
+		$(function(){
+				$(".next ").click(function(){
+					var newUrl = window.location.href.split("#");
+					var id = newUrl[1];
+					var newId = parseInt(id) + 1;
+					window.location.href = newUrl[0] + "#"+newId;
+					$('[name='+id+']').hide();
+					$('[name='+newId+']').show();
+				});
+			});
+		
+	}
+
+	function showScreen(){
+		var id = location.hash.slice(1);
+		var newId = id + 1;
+		$("#"+location.hash.slice(1)).hide();
+		$("#"+newId).show();
 	}
 	
 	function setUpQuiz(){
@@ -41,6 +66,11 @@
 		}else{
 			return "Done.";
 		}
+	}
+	
+	function makeNewScreen(){
+		var nextButton = $(document.createElement('button')).addClass("next_button").addClass("next").text("NEXT");
+		$("#container").append(nextButton);
 	}
 	
 })();
